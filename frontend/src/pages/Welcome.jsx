1f8542e4ad9bee032d97806b675cc2a0b82e4e71
Poojitha <poojithamullapudi86@gmail.com>
2026-06-25 13:46:28 +0530

    as

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -7,7 +7,7 @@ function Welcome({ onStart }) {
             <div className="welcome-card">
 
                 <h1>
-                    Future Self Alignment Platform for 
+                    Future Self Alignment Platform for pooj
                 </h1>
 
 
