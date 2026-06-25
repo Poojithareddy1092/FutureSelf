0cff5631bcf9de5780193a0db34e05cfad302e98
Poojitha <poojithamullapudi86@gmail.com>
2026-06-25 11:35:09 +0530

    1st change

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -14,7 +14,7 @@ function Welcome({ onStart }) {
                 <p>
                     Become the person you aspire to be.
                     <br />
-                    Track your actions. Reflect and refract. Grow.
+                    Track your actions. REFLECT. Grow.
                 </p>
 
 
